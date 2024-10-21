83fb9e2137a240aca2559dff4b46025c8155d9dc
SS-snap <2kganzaoji@gmail.com>
2024-10-21 17:44:22 +0800

    Add a description of what you changed

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from .area_calculator import AreaCalculator
 from .Snap_canvas import PyQtCanvasNode
 from .Snapload import Snapload
+from .canvas_window import CanvasWindow  
 
 VERSION = "1.0"
 
@@ -8,12 +9,14 @@ NODE_CLASS_MAPPINGS = {
     "AreaCalculator": AreaCalculator,
     "PyQtCanvasNode": PyQtCanvasNode,
     "Snapload": Snapload,
+    "CanvasWindow": CanvasWindow,  
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "AreaCalculator": "Snap Area",
     "PyQtCanvasNode": "Snap Canvas",
-    "Snapload": "Snap load", 
+    "Snapload": "Snap load",
+    "CanvasWindow": "Canvas Window",  
 }
 
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'CanvasWindow']  
